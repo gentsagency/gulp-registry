93a3f2c10e25f9d25db554f5d76e20b32e53a189
Pieter Beulque <pieterbeulque@gmail.com>
2018-07-04 11:44:35 +0200

    Update dependencies for ES6 workflow
    Swap rollup-stream for gulp-better-rollup for a better
    integration in the Gulp pipeline architecture.
    
    Update all other dependencies to the latest version.

--- tasks/javascript/es6.js
+++ tasks/javascript/es6.js
@@ -1,33 +1,32 @@
 const gulp = require('gulp');
 
-const babili = require('gulp-babel-minify');
+const betterRollup = require('gulp-better-rollup');
 const buffer = require('vinyl-buffer');
-const rename = require('gulp-rename');
 const commonjs = require('rollup-plugin-commonjs');
+const minify = require('rollup-plugin-babel-minify');
+const rename = require('gulp-rename');
 const resolve = require('rollup-plugin-node-resolve');
-const rollup = require('rollup-stream');
-const tap = require('gulp-tap');
+const rollup = require('rollup');
 const sourcemaps = require('gulp-sourcemaps');
 
 module.exports = (config) => {
 	const { src, dest } = config;
 
 	const es6 = () => gulp.src(src)
-		.pipe(tap((file) => {
-			// eslint-disable-next-line
-			file.contents = rollup({
-				input: file.path,
-				format: 'es',
-				plugins: [
-					resolve({ browser: true }),
-					commonjs({ sourceMap: false }),
-				],
-				sourcemap: true,
-			});
+		.pipe(betterRollup({
+			rollup,
+			plugins: [
+				resolve({ browser: true }),
+				commonjs({ sourceMap: false }),
+				minify({ comments: false, sourceMap: true }),
+			],
+		},
+		{
+			format: 'es',
+			sourcemap: false,
 		}))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
-		.pipe(babili())
 		.pipe(rename({ suffix: '.es6' }))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(dest));
