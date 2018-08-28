beec3a1d244e3722ce79344b47dd4e1b7f87f10e
Pieter Beulque <pieterbeulque@gmail.com>
2018-08-28 10:35:03 +0200

    Fix sourcemaps for ES6 task

--- tasks/javascript/es6.js
+++ tasks/javascript/es6.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 
 const betterRollup = require('gulp-better-rollup');
-const buffer = require('vinyl-buffer');
 const commonjs = require('rollup-plugin-commonjs');
 const minify = require('rollup-plugin-babel-minify');
 const rename = require('gulp-rename');
@@ -13,22 +12,20 @@ module.exports = (config) => {
 	const { src, dest } = config;
 
 	const es6 = () => gulp.src(src)
+		.pipe(sourcemaps.init())
 		.pipe(betterRollup({
 			rollup,
 			plugins: [
 				resolve({ browser: true }),
-				commonjs({ sourceMap: false }),
-				minify({ comments: false, sourceMap: true }),
+				commonjs(),
+				minify({ comments: false }),
 			],
 		},
 		{
 			format: 'es',
-			sourcemap: false,
 		}))
-		.pipe(buffer())
-		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(rename({ suffix: '.es6' }))
-		.pipe(sourcemaps.write('.'))
+		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(dest));
 
 	return es6;
