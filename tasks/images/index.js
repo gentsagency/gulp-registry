859e8f0d6add734dadaf4cc87a60d8fbd4c7a4ac
Pieter Beulque <pieterbeulque@gmail.com>
2018-11-09 08:25:55 +0100

    Generate image sizes (#6)
    * Update dependencies
    
    * Update dependencies
    
    * Generate multiple image sizes
    
    * Fix gulp.lastRun

--- tasks/images/index.js
+++ tasks/images/index.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 
+const filter = require('gulp-filter');
+const gm = require('gulp-gm');
 const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
 
 module.exports = (config) => {
 	const { src, dest } = config;
@@ -9,6 +12,20 @@ module.exports = (config) => {
 		.src(src, { since: gulp.lastRun(images) })
 		.pipe(imagemin())
 		.pipe(gulp.dest(dest));
+
+	if (Array.isArray(config.generateSizes) && config.generateSizes.length > 0) {
+		const allowedGlobs = ['**/*.jpg', '**/*.jpeg', '**/*.png'];
+
+		const resizers = config.generateSizes.map((size) => () => gulp
+			.src(src, { since: gulp.lastRun(images) })
+			.pipe(filter(allowedGlobs))
+			.pipe(gm((image) => image.resize(size)))
+			.pipe(imagemin())
+			.pipe(rename({ suffix: `-${size}` }))
+			.pipe(gulp.dest(dest)));
+
+		return gulp.parallel(images, ...resizers);
+	}
 
 	return images;
 };
